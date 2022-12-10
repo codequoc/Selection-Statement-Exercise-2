fc310f93fbeafe15d23f87206bb7085dbe770ba0
Henry Nguyen <nguyen.henry796@gmail.com>
2022-12-09 22:19:05 -0500

    Exercise 2 done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            string subject = Console.ReadLine();
+
+            switch(subject)
+            {
+                case "math":
+                    Console.WriteLine("You must like problem solving with numbers");
+                    break;
+                case "reading":
+                    Console.WriteLine("You're such a bookworm");
+                    break;
+                case "writing":
+                    Console.WriteLine("You must be creative");
+                    break;
+                case "science":
+                    Console.WriteLine("You must like conducting experiments");
+                    break;
+                case "history":
+                    Console.WriteLine("You must like knowing the past");
+                    break;
+                default:
+                    Console.WriteLine("This is not a subject");
+                    break;
+            }
         }
     }
 }
